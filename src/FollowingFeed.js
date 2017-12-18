92aa0d7be2907d4b97faba561fa22efc6c80b1f7
Connor Turland <cturland@brainstation.io>
2017-12-17 22:07:03 -0500

    ensure FollowingFeed gets posts on 1 sec interval

--- src/FollowingFeed.js
+++ src/FollowingFeed.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import Meow from './Meow'
 
 class FollowingFeed extends Component {
+  componentDidMount() {
+    if (this.props.me) {
+      this.props.getMyFeed()
+      if (this.interval) clearInterval(this.interval)
+      this.interval = setInterval(this.props.getMyFeed, 1000)
+    }
+  }
   componentDidUpdate(prevProps) {
     if (!prevProps.me && this.props.me) {
       this.props.getFollow(this.props.me, "following")
